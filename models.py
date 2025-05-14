42a2b820d2d413489d2cf62c78df5b08bae7e60d
micaelavaucher <micaela.vaucher@gmail.com>
2025-05-14 18:52:55 -0300

    new payador version

--- models.py
+++ models.py
@@ -1,9 +1,51 @@
 """Load models to use them as a narrator and a common-sense oracle in the PAYADOR pipeline."""
 import google.generativeai as genai
+import requests
+import replicate
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+def get_llm(model_name: str = "gemini-1.5-flash") -> object:
+
+    google_models = ["gemini-1.0-pro", "gemini-1.5-pro", "gemini-1.5-flash"]
+    replicate_models = ["meta/meta-llama-3-70b", "meta/meta-llama-3-70b-instruct"]
+
+    model = None
+
+    if model_name in google_models:
+        model = GeminiModel(API_key="GOOGLE_API_KEY", model_name=model_name)
+    elif model_name in replicate_models:
+        model = ReplicateModel(API_key="REPLICATE_API_TOKEN", model_name=model_name)
+
+    return model
+
+
+class ReplicateModel():
+    def __init__ (self, API_key:str, model_name:str = "meta/meta-llama-3-70b-instruct") -> None:
+        self.temperature = 0.7
+        self.model_name = model_name
+
+    def prompt_model(self,system_msg: str, user_msg:str) -> str:
+        """Prompt the Replicate model."""
+
+        system_instructions = f"<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n{system_msg}<|eot_id|><|start_header_id|>user<|end_header_id|>"
+
+        input = {
+            "top_p": 0.1,
+            "min_tokens": 0,
+            "temperature": self.temperature,
+            "prompt": user_msg,
+            "prompt_template": system_instructions + "\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n",
+        }
+
+        output = replicate.run(self.model_name,input=input)
+        
+        return "".join(output)
 
 class GeminiModel():
-    def __init__ (self, api_key_file:str) -> None:
+    def __init__ (self, API_key:str, model_name:str = "gemini-1.0-pro") -> None:
         """"Initialize the Gemini model using an API key."""
         self.safety_settings = [
             {
@@ -27,16 +69,9 @@ class GeminiModel():
                 "threshold": "BLOCK_NONE",
             },
         ]
-        genai.configure(api_key=get_api_key(api_key_file))
-        self.model = genai.GenerativeModel("gemini-2.0-flash")
+        genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+        self.model = genai.GenerativeModel(model_name)
 
-    def prompt_model(self,prompt: str) -> str:
+    def prompt_model(self,system_msg: str, user_msg:str) -> str:
         """Prompt the Gemini model."""
-        return self.model.generate_content(prompt, safety_settings=self.safety_settings).text
-
-def get_api_key(path: str) -> str:
-    """Load an API key from path."""
-    key = ""
-    with open(path) as f:
-        key = f.readline()
-    return key
+        return self.model.generate_content(system_msg + "\n\n" + user_msg, safety_settings=self.safety_settings).text
