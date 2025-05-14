20d607e02c0cd2d17d3b1ab02c53737e70731e36
micaelavaucher <micaela.vaucher@gmail.com>
2025-05-14 18:52:55 -0300

    new payador version

--- world.py
+++ world.py
@@ -5,6 +5,7 @@ and methods to update according to the detected changes by a language model.
 """
 
 import re
+from typing import Type
 
 
 class Component:
@@ -19,6 +20,20 @@ class Component:
 
     self.descriptions = descriptions
     """a set of natural language descriptions for the component"""
+  
+class Puzzle (Component):
+  """A class to represent a Puzzle"""
+
+  def __init__(self, name:str, descriptions: 'list[str]', problem: str, answer: str):
+    
+    super().__init__(name, descriptions)
+    """inherited from Component"""
+
+    self.problem = problem
+    """the main problem to be solved"""
+
+    self.answer = answer
+    """a possible answer to the riddle or puzzle"""
 
 class Item (Component):
   """A class to represent an Item."""
@@ -69,7 +84,7 @@ class Location (Component):
     """
     if self.blocked_locations[location.name]:
       self.connecting_locations += [location]
-      if self.blocked_locations[location.name][2]:
+      if self.blocked_locations[location.name][2] and self not in location.connecting_locations:
         location.connecting_locations += [self]
       del self.blocked_locations[location.name]
     else:
@@ -88,10 +103,15 @@ class Character (Component):
     self.location = location
     """the location of the character"""
 
+    self.visited_locations = {self.location.name: []}
+    """a dictionary that contains the successive descriptions of the visited places"""
+
   def move(self, new_location: Location):
     """Move the character to a new location."""
     if new_location in self.location.connecting_locations:
       self.location = new_location
+      if self.location.name not in self.visited_locations:
+        self.visited_locations[self.location.name] = []
     else:
       raise Exception(f"Error: {new_location.name} is not reachable")
 
@@ -138,6 +158,40 @@ class World:
     self.player = player
     """a character for the player"""
 
+    self.objective = None
+    """the current objective for the player in this world"""
+
+  def set_objective (self, first_component: Type[Component], second_component: Type[Component]):
+    
+    first_component_class = first_component.__class__.__name__
+    second_component_class = second_component.__class__.__name__
+    
+    if first_component_class == "Character" and second_component_class == "Character":
+      self.objective = (first_component, second_component)
+    elif (first_component_class == "Character" and second_component_class in ["Location", "Item"]) or (second_component_class == "Character" and first_component_class in ["Location", "Item"]):
+      self.objective = (first_component, second_component)
+    elif (first_component_class  == "Item" and second_component_class == "Location") or (second_component_class  == "Item" and first_component_class == "Location"):
+      self.objective = (first_component, second_component)
+    else:
+      raise Exception(f"Error: Cannot set objective with classes {first_component_class} and {second_component_class}")
+
+  def check_objective(self) -> bool:
+
+    done = False
+    first_component_class = self.objective[0].__class__.__name__
+    second_component_class = self.objective[1].__class__.__name__
+
+    if first_component_class == "Character" and second_component_class == "Character":
+      if self.objective[0].location == self.objective[1].location: done = True
+    elif first_component_class == "Character" and second_component_class == "Location":
+      if self.objective[0].location == self.objective[1]: done = True
+    elif first_component_class == "Character" and second_component_class == "Item":
+      if self.objective[1] in self.objective[0].inventory: done = True
+    elif first_component_class == "Item" and second_component_class == "Location":
+      if self.objective[0] in self.objective[1].items: done = True
+
+    return done
+
   def add_location (self,location: Location) -> None:
     """Add a location to the world."""
     if location.name in self.locations:
@@ -174,8 +228,90 @@ class World:
     for character in characters:
       self.add_character(character)
 
-  def render_world(self, *,  detail_components:bool = True) -> str:
+  def render_world(self, *, language:str = 'en', detail_components:bool = True) -> str:
     """Return the fictional world as a natural language description, using simple sentences.
+
+    The components described are only those the player can see in the current location.
+    If detail_components is False, then the descriptions for each component are not included.
+    """
+    rendered_world = ''
+
+    if language == 'es':
+      rendered_world = self.__render_world_spanish(detail_components = detail_components)
+    else:
+      rendered_world = self.__render_world_english(detail_components = detail_components)
+
+    return rendered_world
+  
+  def __render_world_spanish(self, *,  detail_components:bool = True) -> str:
+    """Return the fictional world as a natural language description, using simple sentences in Spanish.
+
+    The components described are only those the player can see in the current location.
+    If detail_components is False, then the descriptions for each component are not included.
+    """
+    player_location = self.player.location
+    reachable_locations = [f"<{p.name}>" for p in player_location.connecting_locations]
+    blocked_passages = [f"<{p}> bloqueado por <{player_location.blocked_locations[p][1].name}>" for p in player_location.blocked_locations.keys()]
+    characters_in_the_scene = [character for character in self.characters.values() if character.location is player_location]
+
+    
+    world_description = f'El jugador está en <{player_location.name}>\n'
+    
+    if reachable_locations:
+      world_description += f'Desde <{player_location.name}> el jugador puede ir a: {(", ").join(reachable_locations)}\n'
+    else:
+      world_description += f'Desde <{player_location.name}> el jugador puede ir a: None\n'
+
+    if blocked_passages:
+      world_description += f'Desde <{player_location.name}> hay pasajes bloqueados hacia: {(", ").join(blocked_passages)}\n'
+    else:
+      world_description += f'Desde <{player_location.name}> hay pasajes bloqueados hacia: None\n'
+
+    if self.player.inventory:
+      world_description += f'El jugador tiene los siguientes objetos en su inventario: {(", ").join([f"<{i.name}>" for i in self.player.inventory])}\n'
+    else:
+      world_description += f'El jugador tiene los siguientes objetos en su inventario: None\n'
+
+    if player_location.items:
+      world_description += f'El jugador puede ver los siguientes objetos: {(", ").join([f"<{i.name}>" for i in player_location.items])}\n'
+    else:
+      world_description += f'El jugador puede ver los siguientes objetos: None\n'
+      
+    if characters_in_the_scene:
+      world_description += f'El jugador puede ver a los siguientes personajes: {(", ").join([f"<{c.name}>" for c in characters_in_the_scene])}'
+    else:
+      world_description += f'El jugador puede ver a los siguientes personajes: None'
+
+    details = ""
+    if detail_components:
+      items_in_the_scene = player_location.items + self.player.inventory + [blocked_values[1] for blocked_values in player_location.blocked_locations.values() if isinstance(blocked_values[1], Item)]
+      puzzles_in_the_scene = [blocked_values[1] for blocked_values in player_location.blocked_locations.values() if isinstance(blocked_values[1], Puzzle)]
+
+      details += "\nAquí hay una descripción de cada componente.\n"
+      details += f"<{player_location.name}>: Este es el lugar en el que está el jugador. {('. ').join(player_location.descriptions)}.\n"
+      details += "Personajes:\n"
+      details += f"- <Jugador>: El jugador está actuando como <{self.player.name}>. {('. ').join(self.player.descriptions)}.\n"
+      if len(characters_in_the_scene)>0:
+        for character in characters_in_the_scene:
+          details += f"- <{character.name}>: {('. ').join(character.descriptions)}."
+          if len(character.inventory)>0:
+            details += f"Este personaje tiene los siguientes objetos en su inventario: {(', ').join([f'<{i.name}>' for i in character.inventory])}\n"
+            items_in_the_scene+= character.inventory
+          else:
+            details += "\n"
+      if len(items_in_the_scene)>0:
+        details+="Objetos:\n"
+        for item in items_in_the_scene:
+          details += f"- <{item.name}>: {('. ').join(item.descriptions)}\n"
+      if len(puzzles_in_the_scene)>0:
+        details+="Puzzles:\n"
+        for puzzle in puzzles_in_the_scene:
+          details+= f'- <{puzzle.name}>: {(". ").join(puzzle.descriptions)}. El acertijo a resolver es: "{puzzle.problem}". La respuesta esperada, que NO PUEDES decirle al jugador (JAMÁS) es: "{puzzle.answer}".\n'
+
+    return world_description + '\n' + details
+
+  def __render_world_english(self, *,  detail_components:bool = True) -> str:
+    """Return the fictional world as a natural language description, using simple sentences in English.
 
     The components described are only those the player can see in the current location.
     If detail_components is False, then the descriptions for each component are not included.
@@ -186,12 +322,12 @@ class World:
     characters_in_the_scene = [character for character in self.characters.values() if character.location is player_location]
 
     
-    world_description = f'You are in <{player_location.name}>\n'
+    world_description = f'The player is in <{player_location.name}>\n'
     
     if reachable_locations:
-      world_description += f'From <{player_location.name}> you can access: {(", ").join(reachable_locations)}\n'
+      world_description += f'From <{player_location.name}> the player can access: {(", ").join(reachable_locations)}\n'
     else:
-      world_description += f'From <{player_location.name}> you can access: None\n'
+      world_description += f'From <{player_location.name}> the player can access: None\n'
 
     if blocked_passages:
       world_description += f'From <{player_location.name}> there are blocked passages to: {(", ").join(blocked_passages)}\n'
@@ -199,28 +335,29 @@ class World:
       world_description += f'From <{player_location.name}> there are blocked passages to: None\n'
 
     if self.player.inventory:
-      world_description += f'You have the following items in your inventory: {(", ").join([f"<{i.name}>" for i in self.player.inventory])}\n'
+      world_description += f'The player has the following objects in the inventory: {(", ").join([f"<{i.name}>" for i in self.player.inventory])}\n'
     else:
-      world_description += f'You have the following items in your inventory: None\n'
+      world_description += f'The player has the following objects in the inventory: None\n'
 
     if player_location.items:
-      world_description += f'If you look around, you can see the following items: {(", ").join([f"<{i.name}>" for i in player_location.items])}\n'
+      world_description += f'The player can see the following objects: {(", ").join([f"<{i.name}>" for i in player_location.items])}\n'
     else:
-      world_description += f'If you look around, you can see the following items: None\n'
+      world_description += f'The player can see the following objects: None\n'
       
     if characters_in_the_scene:
-      world_description += f'You can see some people: {(", ").join([f"<{c.name}>" for c in characters_in_the_scene])}'
+      world_description += f'The player can see the following characters: {(", ").join([f"<{c.name}>" for c in characters_in_the_scene])}'
     else:
-      world_description += f'You can see some people: None'
+      world_description += f'The player can see the following characters: None'
 
     details = ""
     if detail_components:
       items_in_the_scene = player_location.items + self.player.inventory + [blocked_values[1] for blocked_values in player_location.blocked_locations.values() if isinstance(blocked_values[1], Item)]
-      
+      puzzles_in_the_scene = [blocked_values[1] for blocked_values in player_location.blocked_locations.values() if isinstance(blocked_values[1], Puzzle)]
+
       details += "\nHere is a description of each component.\n"
       details += f"<{player_location.name}>: This is the player's location. {('. ').join(player_location.descriptions)}.\n"
       details += "Characters:\n"
-      details += f"- <Player>: The player is acting as {self.player.name}. {('. ').join(self.player.descriptions)}.\n"
+      details += f"- <Player>: The player is acting as <{self.player.name}>. {('. ').join(self.player.descriptions)}.\n"
       if len(characters_in_the_scene)>0:
         for character in characters_in_the_scene:
           details += f"- <{character.name}>: {('. ').join(character.descriptions)}."
@@ -233,10 +370,14 @@ class World:
         details+="Objects:\n"
         for item in items_in_the_scene:
           details += f"- <{item.name}>: {('. ').join(item.descriptions)}\n"
+      if len(puzzles_in_the_scene)>0:
+        details+="Puzzles:\n"
+        for puzzle in puzzles_in_the_scene:
+          details+= f'- <{puzzle.name}>: {(". ").join(puzzle.descriptions)}. The riddle to solve is: "{puzzle.problem}". The expected answer, that you CANNOT tell the player (EVER) is: "{puzzle.answer}".\n'
 
     return world_description + '\n' + details
 
-  def parse_updates (self, updates: str) -> None:
+  def update (self, updates: str) -> None:
     """Does the changes in the world according to the output of the language model.
 
     The possible changes considered are:
@@ -264,7 +405,7 @@ class World:
         try:
           world_item = self.items[pair[0][0]]
           
-          if pair[0][1] == 'Inventory': #(save_item case)
+          if pair[0][1] in ['Inventory', 'Inventario', 'Player',  'Jugador', self.player.name]: #(save_item case)
             item_location = [character for character in list(self.characters.values()) if world_item in character.inventory]
             item_location += [location for location in list(self.locations.values()) if world_item in location.items]
             self.player.save_item(world_item, item_location[0])
